358935cc69283cf074ba24a0020e2f8d9492d18a
falihfarhan <falihfarhan99@gmail.com>
2019-09-06 16:25:31 +0700

    initial commit

---
+++ src/Calculator/components/Keypad/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Buttons from './components/Buttons';
+
+class Keypad extends React.Component {
+
+
+    
+    render() {
+        return (
+                <div className='keypad'>
+                    <Buttons/>
+                </div>
+        )
+    }
+}
+
+export default Keypad
---
+++ src/Calculator/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Keypad from './components/Keypad';
+
+class Calculator extends React.Component {
+    render(){
+        return <Keypad />
+    }
+}
+
+export default Calculator
